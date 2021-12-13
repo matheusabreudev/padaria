a51d3f8b57fc774fcf65dfc844fff7527815abac
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2021-12-13 00:03:31 -0300

    implementado o ProdutoEntity

--- src/main/java/com/betatech/padaria/entities/FuncionarioEntity.java
+++ src/main/java/com/betatech/padaria/entities/FuncionarioEntity.java
@@ -8,16 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class FuncionarioEntity implements Serializable{
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/betatech/padaria/entities/ProdutoEntity.java
+++ src/main/java/com/betatech/padaria/entities/ProdutoEntity.java
@@ -1,5 +1,27 @@
 package com.betatech.padaria.entities;
 
-public class ProdutoEntity {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class ProdutoEntity implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+	private int quantidade;
+	private String nome;
+	private String preco;
+	private String descricao;
 }
