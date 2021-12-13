0ca049928a77d2fccd33159374cc9ea59d24c6be
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2021-12-12 23:19:59 -0300

    primeiro commit

---
+++ src/main/java/com/betatech/padaria/PadariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.betatech.padaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betatech/padaria/PadariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betatech.padaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
