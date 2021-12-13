28c48a17d48b6ffc8600960d30f35e4107324e5d
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2021-12-12 23:49:06 -0300

    implementado FuncionarioEntity

--- src/main/java/com/betatech/padaria/entities/FuncionarioEntity.java
+++ src/main/java/com/betatech/padaria/entities/FuncionarioEntity.java
@@ -1,5 +1,35 @@
 package com.betatech.padaria.entities;
 
-public class FuncionarioEntity {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FuncionarioEntity implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private String usuario;
+	private String senha;
+	private String email;
 
 }
